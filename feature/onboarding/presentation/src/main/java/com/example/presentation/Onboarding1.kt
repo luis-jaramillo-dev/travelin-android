cf601e926e01fd468e643d51cf9e5354fd97abb1
Ricardo Cea <ricardocea.dev@gmail.com>
2025-04-12 16:41:10 -0400

    * Creating sub-modules: 	- data 	- domain 	- presentation * Android Studio - Gradle local autoconfiguration-3 * Refactor MyClass presentation module: Onboarding1

---
+++ feature/onboarding/presentation/src/main/java/com/example/presentation/Onboarding1.kt
@@ -0,0 +1,27 @@
+package com.example.presentation
+
+//import android.annotation.SuppressLint
+//import android.os.Bundle
+//import android.util.Log
+//import androidx.activity.ComponentActivity
+//import androidx.activity.compose.setContent
+//import androidx.activity.enableEdgeToEdge
+//import androidx.compose.foundation.layout.Box
+//import androidx.compose.foundation.layout.Column
+//import androidx.compose.foundation.layout.padding
+//import androidx.compose.material3.Button
+//import androidx.compose.material3.MaterialTheme
+//import androidx.compose.material3.Scaffold
+//import androidx.compose.material3.Text
+//import androidx.compose.runtime.Composable
+//import androidx.compose.ui.Modifier
+//import androidx.navigation.compose.NavHost
+//import androidx.navigation.compose.composable
+//import androidx.navigation.compose.rememberNavController
+//import com.projectlab.navigation.NavigationCommand
+//import com.projectlab.navigation.NavigationManager
+
+
+
+class Onboarding1 {
+}
