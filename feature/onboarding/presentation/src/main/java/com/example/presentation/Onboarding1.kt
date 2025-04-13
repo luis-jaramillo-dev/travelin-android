3429557920505531f638d5d3aac270f11a5e2f43
Ricardo Cea <ricardocea.dev@gmail.com>
2025-04-12 21:23:28 -0400

    I generate an -hola android- for Onboarding1_kt file

--- feature/onboarding/presentation/src/main/java/com/example/presentation/Onboarding1.kt
+++ feature/onboarding/presentation/src/main/java/com/example/presentation/Onboarding1.kt
@@ -1,27 +1,41 @@
 package com.example.presentation
 
-//import android.annotation.SuppressLint
-//import android.os.Bundle
-//import android.util.Log
-//import androidx.activity.ComponentActivity
-//import androidx.activity.compose.setContent
-//import androidx.activity.enableEdgeToEdge
-//import androidx.compose.foundation.layout.Box
-//import androidx.compose.foundation.layout.Column
-//import androidx.compose.foundation.layout.padding
-//import androidx.compose.material3.Button
-//import androidx.compose.material3.MaterialTheme
-//import androidx.compose.material3.Scaffold
-//import androidx.compose.material3.Text
-//import androidx.compose.runtime.Composable
-//import androidx.compose.ui.Modifier
-//import androidx.navigation.compose.NavHost
-//import androidx.navigation.compose.composable
-//import androidx.navigation.compose.rememberNavController
-//import com.projectlab.navigation.NavigationCommand
-//import com.projectlab.navigation.NavigationManager
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.FlowPreview
 
-
-class Onboarding1 {
+// Hola Android
+@Composable
+fun Onboarding1( modifier: Modifier = Modifier) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Hola Android",
+            fontSize = 24.sp
+        )
+    }
 }
+
+@Preview(showBackground = true)
+@Composable
+fun Onboarding1Preview() {
+    Onboarding1( modifier = Modifier.fillMaxSize() )
+}
+
+
+
