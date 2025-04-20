f125a4506c3904166555e5b0b17ef02898f5f2ee
Mauricio Fuentes <mauriciofb@gmail.com>
2025-04-20 18:59:33 -0400

    Gradle Android Compose Convention Plugin
    Signed-off-by: mfuent <mauriciofb@gmail.com>

--- build-logic/convention/src/main/java/AndroidApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/java/AndroidApplicationConventionPlugin.kt
@@ -1,21 +1,31 @@
 import com.android.build.api.dsl.ApplicationExtension
+import com.kotlinpl.convention.ExtensionType
+import com.kotlinpl.convention.configureBuildTypes
 import com.kotlinpl.convention.configureKotlinAndroid
+import com.kotlinpl.convention.libs
+import com.kotlinpl.convention.version
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.configure
-import org.gradle.kotlin.dsl.getByType
 
 
 class AndroidApplicationConventionPlugin : Plugin <Project> {
     override fun apply(target: Project) {
         with(target) {
             apply(plugin = "com.android.application")
-            apply(plugin = "org.jetbrains.kotlin.android")
+            apply(plugin = "kotlin-android")
 
             extensions.configure<ApplicationExtension> {
                 configureKotlinAndroid(this)
-                defaultConfig.targetSdk = 35
+                defaultConfig.targetSdk = libs.version("projectTargetSdk").toInt()
+
+                /**
+                 * Used for api key config on project
+                 *
+                 * must add API_KEY = "your key" on local.properties
+                 */
+                configureBuildTypes(this, ExtensionType.APPLICATION)
             }
         }
     }
