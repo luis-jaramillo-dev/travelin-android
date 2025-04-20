a77ed4e8c97e92522d86f4b73d74cd465ccea23a
Joaquin Ossandon <joaquinossandong@gmail.com>
2025-04-20 19:53:02 -0400

    feat: add compose UI components and configure Compose
    New composable UI components:
    - `BadgeOutline`
    - `GradientBackground`
    - `ButtonPrimary`
    - `ButtonSecondary`
    - `ButtonTertiary`
    - `ButtonOutline`
    
    Configured Jetpack Compose in the `designsystem` module.
    Additionally, integrated and previewed these components in the main activity of the app.
    
    Signed-off-by: Joaquin Ossandon <joaquinossandong@gmail.com>

--- app/src/main/java/com/projectlab/travelin_android/MainActivity.kt
+++ app/src/main/java/com/projectlab/travelin_android/MainActivity.kt
@@ -9,7 +9,14 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.projectlab.core.presentation.designsystem.BadgeOutline
+import com.projectlab.core.presentation.designsystem.ButtonOutline
+import com.projectlab.core.presentation.designsystem.ButtonPrimary
+import com.projectlab.core.presentation.designsystem.ButtonSecondary
+import com.projectlab.core.presentation.designsystem.ButtonTertiary
+import com.projectlab.core.presentation.designsystem.GradientBackground
 import com.projectlab.travelin_android.ui.theme.TravelinTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +26,9 @@ class MainActivity : ComponentActivity() {
             TravelinTheme(dynamicColor = true) {
                 Scaffold(
                     content = { padding ->
-                        Column(Modifier.padding(padding).padding(horizontal = 16.dp)) {
+                        Column(Modifier
+                            .padding(padding)
+                            .padding(horizontal = 16.dp)) {
                             ExampleUI()
                         }
                     }
@@ -33,3 +42,66 @@ class MainActivity : ComponentActivity() {
 fun ExampleUI() {
     Text("Hello World!")
 }
+
+@Preview(showBackground = true)
+@Composable
+fun GradientPreview() {
+    TravelinTheme(dynamicColor = false, darkTheme = false) {
+        GradientBackground {
+            Text("¡Hola, quiuboles!")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ButtonPrimaryPreview() {
+    TravelinTheme(dynamicColor = false, darkTheme = false) {
+        ButtonPrimary(
+            text = "Primary Button",
+            onClick = {},
+        )
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFF000000)
+@Composable
+fun ButtonSecondaryPreview() {
+    TravelinTheme(dynamicColor = false, darkTheme = false) {
+        ButtonSecondary(
+            text = "Secondary Button",
+            onClick = {},
+        )
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFFFFFFF)
+@Composable
+fun ButtonTertiaryPreview() {
+    TravelinTheme(dynamicColor = false, darkTheme = false) {
+        ButtonTertiary(
+            text = "Tertiary Button",
+            onClick = {},
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ButtonOutlinePreview() {
+    TravelinTheme(dynamicColor = false, darkTheme = false) {
+        ButtonOutline(
+            text = "Outline Button",
+            onClick = {},
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BadgeOutlinePreview() {
+    TravelinTheme(dynamicColor = false, darkTheme = false) {
+        BadgeOutline("2 day 1 night")
+    }
+}
+
