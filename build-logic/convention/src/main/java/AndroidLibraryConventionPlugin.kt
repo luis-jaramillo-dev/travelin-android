01baad8aa8c42e39142f5dbee3d0848ebfba5a38
mfuent <mauriciofb@gmail.com>
2025-04-21 04:00:17 -0400

    Gradle Android Library Compose Convention Plugin
    Signed-off-by: mfuent <mauriciofb@gmail.com>

---
+++ build-logic/convention/src/main/java/AndroidLibraryConventionPlugin.kt
@@ -0,0 +1,42 @@
+import com.android.build.api.dsl.LibraryExtension
+import com.kotlinpl.convention.ExtensionType
+import com.kotlinpl.convention.configureBuildTypes
+import com.kotlinpl.convention.configureKotlinAndroid
+import com.kotlinpl.convention.getLibrary
+import com.kotlinpl.convention.libs
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.apply
+import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.kotlin
+
+class AndroidLibraryConventionPlugin: Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            apply(plugin = "android-library")
+            apply(plugin = "kotlin-android")
+
+            extensions.configure<LibraryExtension> {
+                configureKotlinAndroid(this)
+
+                configureBuildTypes(
+                    commonExtension = this,
+                    extensionType = ExtensionType.LIBRARY
+                )
+
+                defaultConfig {
+                    minSdk = 24
+
+                    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+                    consumerProguardFiles("consumer-rules.pro")
+                }
+            }
+
+            dependencies {
+                "testImplementation"(kotlin("test"))
+            }
+
+        }
+    }
+}
