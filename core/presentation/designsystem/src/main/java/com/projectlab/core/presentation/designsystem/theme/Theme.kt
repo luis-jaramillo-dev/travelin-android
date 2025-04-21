e510d0e54bdd3ad6c7d706edca5bb13aec656ebe
Joaquin Ossandon <joaquinossandong@gmail.com>
2025-04-21 11:36:06 -0400

    Refactor: designsystem and creating badge component
    Signed-off-by: Joaquin Ossandon <joaquinossandong@gmail.com>

--- core/presentation/designsystem/src/main/java/com/projectlab/core/presentation/designsystem/theme/Theme.kt
+++ core/presentation/designsystem/src/main/java/com/projectlab/core/presentation/designsystem/theme/Theme.kt
@@ -1,4 +1,4 @@
-package com.projectlab.travelin_android.ui.theme
+package com.projectlab.core.presentation.designsystem.theme
 
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
