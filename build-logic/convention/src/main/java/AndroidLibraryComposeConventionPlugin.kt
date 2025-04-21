0243339426a76bfde8151284365269deddfc3fe3
mfuent <mauriciofb@gmail.com>
2025-04-21 04:00:17 -0400

    Gradle Android Library Compose Convention Plugin
    Signed-off-by: mfuent <mauriciofb@gmail.com>

---
+++ build-logic/convention/src/main/java/AndroidLibraryComposeConventionPlugin.kt
@@ -0,0 +1,19 @@
+import com.android.build.api.dsl.LibraryExtension
+import com.kotlinpl.convention.configureAndroidCompose
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.apply
+import org.gradle.kotlin.dsl.getByType
+
+class AndroidLibraryComposeConventionPlugin: Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            apply(plugin = "travelinandroid.android.library")
+
+            val extensions = extensions.getByType<LibraryExtension>()
+
+            configureAndroidCompose(extensions)
+
+        }
+    }
+}
