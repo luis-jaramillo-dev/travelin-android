0d8bade7c0cb98a2fcd7ee3b33354ca419698a06
Ricardo Cea <ricardocea.dev@gmail.com>
2025-04-22 03:09:10 -0400

    include preview in file OnboardingPageView.kt, besides I modified MainActivity

--- feature/onboarding/presentation/src/main/java/com/projectlab/feature/onboarding/presentation/ui/OnboardingPageView.kt
+++ feature/onboarding/presentation/src/main/java/com/projectlab/feature/onboarding/presentation/ui/OnboardingPageView.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.projectlab.feature.onboarding.presentation.R
 import com.projectlab.feature.onboarding.presentation.data.OnboardingPage
@@ -95,4 +96,17 @@ fun OnboardingPageView(
             }
         }
     }
+}
+
+@Preview (showBackground = true)
+@Composable
+fun OnboardingPageViewPreview() {
+    OnboardingPageView(
+        page = OnboardingPage(
+            title = "Welcome to Travelin",
+            description = "Discover the world with us",
+            backgroundImage = painterResource(id = R.drawable.onboarding1)
+        ),
+        onNextClicked = {}
+    )
 }
