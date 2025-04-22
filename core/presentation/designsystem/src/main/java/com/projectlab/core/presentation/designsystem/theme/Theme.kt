591201df0a511f3fa833ea6f3003b0cffe93d79b
Pixoll <pixoll.gamer@gmail.com>
2025-04-22 13:00:51 -0400

    Disable dynamicColor by default

--- core/presentation/designsystem/src/main/java/com/projectlab/core/presentation/designsystem/theme/Theme.kt
+++ core/presentation/designsystem/src/main/java/com/projectlab/core/presentation/designsystem/theme/Theme.kt
@@ -105,7 +105,7 @@ val unspecified_scheme = ColorFamily(
 fun TravelinTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable() () -> Unit
 ) {
     val colorScheme = when {
