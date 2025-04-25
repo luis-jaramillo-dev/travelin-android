a0a83f9737f01458fcfe8ac67252bf2807ff474c
Rocío <rocio.diaz-sm@hotmail.com>
2025-04-24 21:44:10 -0400

    Error resuelto

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,43 @@
 plugins {
 //    alias(libs.plugins.travelinandroid.android.application)
-    alias(libs.plugins.travelinandroid.android.application.compose)
+   // alias(libs.plugins.travelinandroid.android.application.compose)
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
 }
 
 android {
     namespace = "com.projectlab.travelin_android"
 
+    compileSdk = 35
+
     defaultConfig {
+        applicationId = "com.projectlab.travelin_android"
+        minSdk = 24
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.14"
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 
     kotlinOptions {
@@ -18,7 +45,7 @@ android {
     }
 }
 
-dependencies {
+/*dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -27,6 +54,25 @@ dependencies {
     // core system
     implementation(projects.core.presentation.designsystem)
     implementation(projects.core.presentation.ui)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+}*/
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.compose.google.fonts)
+    // core system
+    implementation(projects.core.presentation.designsystem)
+    implementation(projects.core.presentation.ui)
+    // compose
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    //implementation(libs.androidx.ui.tooling.preview.android)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
--- auth/data/build.gradle.kts
+++ auth/data/build.gradle.kts
@@ -1,9 +1,38 @@
 plugins {
-    alias(libs.plugins.travelinandroid.android.library)
+
+// alias(libs.plugins.travelinandroid.android.library)
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+
+    //google services
+    //alias(libs.plugins.google.services)
+
 }
 
 android {
     namespace = "com.projectlab.auth.data"
+    compileSdk = 35
+    defaultConfig {
+        minSdk = 24
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
 }
 
 dependencies {
