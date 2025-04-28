86f8e15f3b37033004a2a86974bcafb50730cf0a
Pedro Cerda Lara <pefacel@gmail.com>
2025-04-28 13:59:12 -0400

    build: add KSP plugin in module level

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,8 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
+
+    // Ksp
+    alias(libs.plugins.devtools.ksp) apply false
+
 }
