764ec03b1c05ffc7d6b6aa2ccc2136ae993cadfb
Ricardo Cea <ricardocea.dev@gmail.com>
2025-04-28 23:56:36 -0400

    adding test files

---
+++ feature/onboarding/presentation/src/androidTest/java/com/projectlab/feature/onboarding/presentation/ui/OnboardingScreen.kt
@@ -0,0 +1,45 @@
+package com.projectlab.feature.onboarding.presentation.ui
+
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.*
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.projectlab.feature.onboarding.presentation.R
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.security.KeyStore
+import org.junit.Assert.assertTrue
+
+@RunWith(AndroidJUnit4::class)
+class OnboardingScreen {
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    //    @get:Rule
+    //    val composeTestRule = createComposeRule() // TODO: Fix this to use the correct rule
+
+    // TODO: Test needs to be checked
+    @Test
+    fun onboardingScreen_navigatesOnLastPage() {
+        var navigated = false
+
+        composeTestRule.setContent {
+            OnboardingScreen(
+                onNavigateToLogin = { navigated = true }
+            )
+        }
+
+        // Simulate 3 clicks (for 3 pages)
+        repeat(3) {
+            composeTestRule.onNodeWithTag("OnboardingNextButton").performClick()
+        }
+
+        assertTrue(navigated)
+
+    }
+
+}
