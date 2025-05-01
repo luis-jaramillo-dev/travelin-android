cb90182f431d9795b5c34830c64b42ee8123a619
Pedro Cerda Lara <pefacel@gmail.com>
2025-05-01 01:35:24 -0400

    feat: implement Users collections

--- core/data/src/main/java/com/projectlab/core/data/di/DataModule.kt
+++ core/data/src/main/java/com/projectlab/core/data/di/DataModule.kt
@@ -1,8 +1,12 @@
 package com.projectlab.core.data.di
 
+import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.projectlab.core.data.Constants.REFERENCE_USERS
+import com.projectlab.core.data.networking.UsersRepositoryImpl
+import com.projectlab.core.domain.repository.UsersRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -14,5 +18,11 @@ class DataModule {
 
     @Provides
     fun provideFirebaseFirestore(): FirebaseFirestore = Firebase.firestore
+
+    @Provides
+    fun provideUsersRef(db: FirebaseFirestore): CollectionReference = db.collection(REFERENCE_USERS)
+
+    @Provides
+    fun provideUsersRepository(impl: UsersRepositoryImpl): UsersRepository = impl
 
 }
