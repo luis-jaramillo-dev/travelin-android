9c53d1f3ae476c7f5ca50ed0bb0253b3e95a9429
Ricardo Cea <ricardocea.dev@gmail.com>
2025-05-11 19:06:04 -0400

    I modiefied files 'FirestoreModule.kt' and 'UsersRepositoryImpl.kt'

--- core/data/src/main/java/com/projectlab/core/data/di/FirestoreModule.kt
+++ core/data/src/main/java/com/projectlab/core/data/di/FirestoreModule.kt
@@ -1,8 +1,10 @@
-package com.projectlab.core.database.di
+package com.projectlab.core.data.di
 
 import com.google.firebase.firestore.FirebaseFirestore
 import com.projectlab.core.data.networking.FirestoreUserRepositoryImpl
+import com.projectlab.core.data.networking.FirestoreItineraryRepositoryImpl
 import com.projectlab.core.domain.repository.UserRepository
+import com.projectlab.core.domain.repository.ItineraryRepository
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
@@ -17,6 +19,11 @@ abstract class RepositoryModule {
     abstract fun bindUserRepository(
         impl: FirestoreUserRepositoryImpl
     ): UserRepository
+
+    @Binds
+    abstract fun bindItineraryRepository(
+        impl: FirestoreItineraryRepositoryImpl
+    ): ItineraryRepository
 
     // TODO: implement: bind ItineraryRepository, FlightRepository, etc.
 }
--- core/data/src/main/java/com/projectlab/core/data/networking/UsersRepositoryImpl.kt
+++ core/data/src/main/java/com/projectlab/core/data/networking/UsersRepositoryImpl.kt
@@ -3,6 +3,7 @@ package com.projectlab.core.data.networking
 import com.google.firebase.firestore.CollectionReference
 import com.projectlab.core.domain.model.Response
 import com.projectlab.core.domain.entity.UserEntity
+import com.projectlab.core.domain.model.EntityId
 import com.projectlab.core.domain.repository.UsersRepository
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
@@ -15,7 +16,7 @@ class UsersRepositoryImpl @Inject constructor(private val usersRef: CollectionRe
 
     override suspend fun create(userEntity: UserEntity): Response<Boolean> {
         return try {
-            usersRef.document(userEntity.id).set(userEntity).await()
+            usersRef.document(userEntity.id?.value ?: "").set(userEntity).await()
             Response.Success(true)
         } catch (e: Exception) {
             e.printStackTrace()
@@ -26,7 +27,7 @@ class UsersRepositoryImpl @Inject constructor(private val usersRef: CollectionRe
     override fun getUserById(id: String): Flow<UserEntity> = callbackFlow {
         val snapshotListener = usersRef.document(id).addSnapshotListener { snapshot, e ->
             val userEntity = snapshot?.toObject(UserEntity::class.java) ?: UserEntity(
-                id = "",
+                id = EntityId(""),
                 email = "",
                 age = "",
                 firstName = "",
