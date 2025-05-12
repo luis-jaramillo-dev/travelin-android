32c864194cf7efae95020e2bba4987a8ce6a5318
Ricardo Cea <ricardocea.dev@gmail.com>
2025-05-11 20:24:41 -0400

    refactoring dagger-hilt implementation of DataModule.kt: in progress...

--- core/data/src/main/java/com/projectlab/core/data/di/DataModule.kt
+++ core/data/src/main/java/com/projectlab/core/data/di/DataModule.kt
@@ -5,18 +5,39 @@ import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.projectlab.core.data.Constants.REFERENCE_USERS
+import com.projectlab.core.data.networking.FirestoreItineraryRepositoryImpl
+import com.projectlab.core.data.networking.FirestoreUserRepositoryImpl
 import com.projectlab.core.data.networking.UsersRepositoryImpl
+import com.projectlab.core.domain.repository.ItineraryRepository
+import com.projectlab.core.domain.repository.UserRepository
 import com.projectlab.core.domain.repository.UsersRepository
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
-class DataModule {
+abstract class DataModule {
 
-    @Provides
+    @Binds
+    abstract fun bindUserRepository(
+        impl: FirestoreUserRepositoryImpl
+    ): UserRepository
+
+    @Binds
+    abstract fun bindItineraryRepository(
+        impl: FirestoreItineraryRepositoryImpl
+    ): ItineraryRepository
+
+    // TODO: implement: bind ItineraryRepository, FlightRepository, etc.
+
+    companion object {
+
+    }
+    @Provides @Singleton
     fun provideFirebaseFirestore(): FirebaseFirestore = Firebase.firestore
 
     @Provides
