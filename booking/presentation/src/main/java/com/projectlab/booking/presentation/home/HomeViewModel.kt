c93bf98066bd1909635aab3bd5ed22aac7dffa5f
Pixoll <pixoll.gamer@gmail.com>
2025-05-27 11:22:11 -0400

    fix: ensure search queries are not saved more than once

--- booking/presentation/src/main/java/com/projectlab/booking/presentation/home/HomeViewModel.kt
+++ booking/presentation/src/main/java/com/projectlab/booking/presentation/home/HomeViewModel.kt
@@ -3,13 +3,15 @@ package com.projectlab.booking.presentation.home
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.projectlab.core.data.usecase.GetActivitiesUseCase
-import com.projectlab.core.domain.proto.SearchHistory
 import com.projectlab.core.domain.proto.SearchHistory.HistoryType
 import com.projectlab.core.domain.repository.SearchHistoryProvider
 import com.projectlab.core.presentation.ui.utils.ErrorMapper
 import com.projectlab.core.presentation.ui.utils.LocationUtils
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -18,17 +20,19 @@ class HomeViewModel @Inject constructor(
     private val getActivitiesUseCase: GetActivitiesUseCase,
     private val locationUtils: LocationUtils,
     private val errorMapper: ErrorMapper,
-    private val historyProvider: SearchHistoryProvider
+    private val historyProvider: SearchHistoryProvider,
 ) : ViewModel() {
 
     private val _uiState = MutableStateFlow(HomeUiState())
     val uiState: StateFlow<HomeUiState> = _uiState.asStateFlow()
 
+    // TODO ensure it's updated when going back from SearchActivity
     init {
         // Load search history when the ViewModel is created
         viewModelScope.launch {
+            historyProvider.clearSearchHistory(HistoryType.ACTIVITY)
             val list = historyProvider.getSearchHistory(HistoryType.ACTIVITY)
-            _uiState.update { it.copy(history = list.takeLast(3).reversed()) }
+            _uiState.update { it.copy(history = list.reversed()) }
         }
     }
 
@@ -39,14 +43,12 @@ class HomeViewModel @Inject constructor(
     fun onSearchPressed() {
         if (_uiState.value.query.isBlank()) return
         viewModelScope.launch {
-            historyProvider.addSearchEntry(HistoryType.ACTIVITY, _uiState.value.query)
-            val updated = historyProvider.getSearchHistory(HistoryType.ACTIVITY)
-            _uiState.update { it.copy(history = updated.takeLast(3).reversed()) }
+            val updated = historyProvider.addSearchEntry(HistoryType.ACTIVITY, _uiState.value.query)
+            _uiState.update { it.copy(history = updated.reversed()) }
         }
     }
 
     fun clearQuery() {
         _uiState.update { it.copy(query = "") }
     }
-
 }
