7ff3dd9a01015c078a782be3c11929171de14d1c
Pixoll <pixoll.gamer@gmail.com>
2025-06-02 16:51:59 -0400

    feat: improve star rating bar by adding half stars

--- core/presentation/designsystem/src/main/java/com/projectlab/core/presentation/designsystem/component/StarRatingBar.kt
+++ core/presentation/designsystem/src/main/java/com/projectlab/core/presentation/designsystem/component/StarRatingBar.kt
@@ -5,8 +5,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectableGroup
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -14,57 +12,66 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.projectlab.core.presentation.designsystem.R
 import com.projectlab.core.presentation.designsystem.theme.spacing
+import kotlin.math.ceil
 
 @Composable
 fun StarRatingBar(
-    maxStars: Int = 5,
     rating: Float,
-    textColor: Color
+    modifier: Modifier = Modifier,
+    starSize: Dp = 12.dp,
+    spaceBetween: Dp = MaterialTheme.spacing.extraSmall,
+    showRatingText: Boolean = true,
+    textColor: Color = MaterialTheme.colorScheme.onPrimary,
+    maxStars: Int = 5,
 ) {
+    val star = painterResource(R.drawable.star_24px)
+    val starHalf = painterResource(R.drawable.star_half_24px)
+    val starFull = painterResource(R.drawable.star_full_24px)
 
     Row(
-        modifier = Modifier.selectableGroup(),
-        verticalAlignment = Alignment.CenterVertically
+        modifier = modifier.selectableGroup(),
+        verticalAlignment = Alignment.CenterVertically,
     ) {
         for (i in 1..maxStars) {
-            val isSelected = i <= rating
-            val icon = if (isSelected) Icons.Filled.Star else Icons.Default.Star
-            val iconTintColor = if (isSelected) Color(0xFFFFB23F) else Color(0xFFDEDEDE)
+            val icon = when {
+                i <= rating -> starFull
+                i == ceil(rating).toInt() -> starHalf
+                else -> star
+            }
+
             Icon(
-                imageVector = icon,
+                painter = icon,
                 contentDescription = null,
-                tint = iconTintColor,
-                modifier = Modifier.size(12.dp)
+                tint = Color(0xFFFFB23F),
+                modifier = Modifier.size(starSize)
             )
 
             if (i < maxStars) {
-                Spacer(modifier = Modifier.width(MaterialTheme.spacing.extraSmall))
+                Spacer(modifier = Modifier.width(spaceBetween))
             }
-
-        }
-        Spacer(modifier = Modifier.width(10.dp))
-
-        if (rating > 0) {
-            Text(
-                color = textColor,
-                text = rating.toString(),
-                fontSize = 12.sp
-            )
-
-        } else {
-            Text(
-                color = textColor,
-                text = stringResource(R.string.tour_list_card_no_reviews),
-                fontSize = 12.sp
-            )
         }
 
+        if (showRatingText) {
+            Spacer(modifier = Modifier.width(10.dp))
+
+            Text(
+                color = textColor,
+                text = if (rating > 0) {
+                    rating.toString()
+                } else {
+                    stringResource(R.string.tour_list_card_no_reviews)
+                },
+                fontSize = 12.sp,
+            )
+        }
     }
 }
 
