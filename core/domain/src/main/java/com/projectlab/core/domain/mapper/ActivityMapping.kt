5bb320092a4994a3cec2b866d1e68a5533bd5be6
Ricardo Cea <ricardocea.dev@gmail.com>
2025-06-06 02:16:49 -0400

    adding KDoc to ActivityMapping (:core:domain.mapper)

--- core/domain/src/main/java/com/projectlab/core/domain/mapper/ActivityMapping.kt
+++ core/domain/src/main/java/com/projectlab/core/domain/mapper/ActivityMapping.kt
@@ -5,6 +5,15 @@ import com.projectlab.core.domain.model.Activity
 import com.projectlab.core.domain.model.EntityId
 import java.time.Instant
 
+/**
+ * Maps an [Activity] to an [ActivityEntity].
+ * This in order to convert the API model to the database model.
+ *
+ * @param userId The ID of the user associated with the activity.
+ * @param itineraryId The ID of the itinerary associated with the activity.
+ * @return An [ActivityEntity] representing the activity.
+ */
+
 fun Activity.toEntity (
     userId: String,
     itineraryId: String,
