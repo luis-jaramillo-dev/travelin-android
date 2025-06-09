eaa2d782ae8c8cfd08c046ca9c5f38a40eb47773
Pedro Cerda Lara <pefacel@gmail.com>
2025-06-09 15:55:48 -0400

    feat: add string value sealed class to get string resources inside of a view model

--- booking/presentation/src/main/java/com/projectlab/booking/presentation/booking/hotels/BookingHotelState.kt
+++ booking/presentation/src/main/java/com/projectlab/booking/presentation/booking/hotels/BookingHotelState.kt
@@ -1,14 +1,14 @@
 package com.projectlab.booking.presentation.booking.hotels
 
-import com.projectlab.core.domain.model.Hotel
+import com.projectlab.booking.models.HotelUi
 
 data class BookingHotelState(
     val isLoading: Boolean = false,
     val error: String? = null,
-    val currentHotel: Hotel? = null,
     var guestName: String = "",
     var guestNumber: String = "",
     var countryCode: String = "",
     var email: String = "",
-    var idNumber: Number? = null
+    var idNumber: Number? = null,
+    val hotelUi: HotelUi? = null
 )
