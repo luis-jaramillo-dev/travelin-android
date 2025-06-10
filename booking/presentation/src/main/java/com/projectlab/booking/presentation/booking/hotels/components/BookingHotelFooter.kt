3f55d3950bc523323bb7fee12017594eaa63f7ab
PedroSE <65473789+pefacel@users.noreply.github.com>
2025-06-10 12:52:57 -0400

    Pefacel/kot 87 hotels booking (#122)
    * feat: basic screens for booking hotel and successful
    
    * feat: basic nav for booking hotel and successful
    
    * feat: create booking hotel state
    
    * feat: create date picker bottom sheet component
    
    * feat: add navigation from hotel detail to booking
    
    * feat: add string value sealed class to get string resources inside of a view model
    
    * feat: successful hotel booking screen
    
    * feat: hotel booking screen
    
    * refactor: apply string resources in details hotel
    
    * fix: update min sdk from 24 to 26

--- booking/presentation/src/main/java/com/projectlab/booking/presentation/booking/hotels/components/BookingHotelFooter.kt
+++ booking/presentation/src/main/java/com/projectlab/booking/presentation/booking/hotels/components/BookingHotelFooter.kt
@@ -1,4 +1,4 @@
-package com.projectlab.booking.presentation.screens.hotels.details.components
+package com.projectlab.booking.presentation.booking.hotels.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
@@ -10,20 +10,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.projectlab.booking.models.HotelUi
-import com.projectlab.booking.presentation.R
 import com.projectlab.core.presentation.designsystem.component.ButtonComponent
 import com.projectlab.core.presentation.designsystem.component.ButtonVariant
 import com.projectlab.core.presentation.designsystem.theme.spacing
 
 @Composable
-fun DetailHotelBottomBar(
+fun BookingHotelFooter(
     modifier: Modifier = Modifier,
     hotelUi: HotelUi,
-    onClickBookingHotel: () -> Unit
+    confirmBooking: () -> Unit
 ) {
     Row(
         modifier = Modifier
@@ -37,14 +35,14 @@ fun DetailHotelBottomBar(
         verticalAlignment = Alignment.CenterVertically
     ) {
         Text(
-            text = stringResource(R.string.price_per_night, hotelUi.price.formatted),
+            text = "${hotelUi.price.formatted} /per night",
             fontSize = 18.sp,
             fontWeight = FontWeight.W600,
             color = MaterialTheme.colorScheme.primary
         )
         ButtonComponent(
-            text = stringResource(R.string.book_now),
-            onClick = { onClickBookingHotel() },
+            text = "Book now!",
+            onClick = { confirmBooking() },
             variant = ButtonVariant.Primary,
         )
     }
